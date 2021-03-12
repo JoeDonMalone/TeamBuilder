c68a118d8a427d8c49e7f9e96d703818527bdbc9
Joe Malone <joe.d.malone@gmail.com>
2021-03-12 11:36:49 -0600

    some basic html structure with stylings added

---
+++ index.js
@@ -0,0 +1,10 @@
+var managerCard =  `            
+<div id = 'manager' class="card text-white bg-primary mb-5" style="max-width: 18rem;">
+    <div class="future-date"></div>
+    <div class="card-body">
+        <h5 class="future-outlook-image"><img src='#' class = "five-day-img"></h5>
+        <h5 class="future-temperature"></h5>  
+        <h5 class="future-humidity"></h5>  
+    </div>
+</div>
+`
